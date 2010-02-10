43832c2c77ccf0314858f46417868e0d42eac863
unknown <liuchaoyu@.(none)>
2010-02-10 09:00:05 +0800

    Added user's borrowing list synchronization.

--- library.py
+++ library.py
@@ -19,6 +19,7 @@ import urllib2
 from urllib import urlencode
 from HTMLParser import HTMLParser
 import re
+import base64
 
 STATE_LOC = 1
 STATE_INQ_NO = 2
@@ -50,7 +51,113 @@ def parse_index(idx):
     else:
         return None, None
 
+class Stack:
+    def __init__(self, initial=None):
+        self._stack = []
+        self.count = 0
+        
+        if initial:
+            self._stack.extend(initial)
+            self.count = len(self._stack)
+            
+    def push(self, val):
+        self.count += 1
+        self._stack.append(val)
+        
+    def pop(self):
+        if not self.count:
+            return None
+        
+        val = self._stack.pop()
+        self.count -= 1
+        return val
+        
+    def top(self):
+    	if self.count == 0:
+    		return None
+        return self._stack[self.count - 1]
+        
+    def truncate(self):
+        self._stack = []
+        self.count = 0
+
+class UserListParser(HTMLParser):
+    """Parse the HTML body of specific user's borrowing list"""
+    def initialize(self):
+        self.stack = Stack()
+        self.user_list = []
+        self.book = {}
+        
+    def handle_starttag(self, tag, attrs):
+        if tag == 'tr' and ('class', 'patFuncEntry') in attrs:
+            self.stack.push('tr.patFuncEntry')
+            return
+        elif tag == 'td' and self.stack.top() == 'tr.patFuncEntry':
+            td_class = self.get_attr('class', attrs)
+            if td_class in ('patFuncMark', 'patFuncTitle', 'patFuncBarcode', 'patFuncStatus', 'patFuncCallNo'):
+                self.stack.push(".".join([tag, td_class]))
+                return
+        elif tag == 'input' and self.stack.top() == 'td.patFuncMark':
+            self.book.setdefault('renew_id', self.get_attr('value', attrs))
+        elif tag == 'a' and self.stack.top() == 'td.patFuncTitle':
+            self.book.setdefault('entry', self.get_attr('href', attrs))
+        
+        self.stack.push(tag)
+        
+    def handle_endtag(self, tag):
+        if tag == 'tr' and self.stack.top() == 'tr.patFuncEntry':
+            self.user_list.append(dict(self.book))
+            self.book = {} # reset 
+        
+        self.stack.pop()
+        
+    def handle_data(self, data):
+        if self.stack.top()[:3] == 'td.':
+            td, td_class = self.stack.top().split('.')
+            data = data.strip()
+            
+            if td_class in ('patFuncBarcode', 'patFuncStatus', 'patFuncCallNo'):
+                tag = td_class[7:].lower()
+                self.book.setdefault(tag, data)
+                
+    def get_attr(self, name, attrs):
+        for attr_name, value in attrs:
+            if name == attr_name:
+                return value
+                
+        return None
+
+class LibISBNParser(HTMLParser):
+    def initialize(self):
+        self.isbn = ''
+        self.state = None
+        self.stack = Stack()
+        self.r_field_tag = re.compile('fieldtag\=(\w)')
+        
+    def handle_starttag(self, tag, attrs):
+        if tag == 'td' and ('class', 'bibInfoData') in attrs:
+            self.stack.push("td.bibInfoData")
+            return
+            
+        self.stack.push(tag)
+        
+    def handle_comment(self, data):
+        srch = self.r_field_tag.search(data)
+        if srch.groups():
+            self.state = srch.groups()[0]
+            
+    def handle_data(self, data):
+        if self.stack.top() == 'td.bibInfoData' and self.state == 'i':
+            data = data.strip()
+            pos = data.find('CNY')
+            if pos > -1:
+                self.isbn = re.sub('-', '', data[:pos-1])
+        
+    def handle_endtag(self, tag):
+        self.stack.pop()
+        
 class LibReservParser(HTMLParser):
+    """Parse the reservation information (represented in HTML), and generates metadata for specific books"""
     STATES = {'field 1': STATE_LOC, 'field C': STATE_INQ_NO, 'field %': STATE_STATUS, 'field b': STATE_BARCODE, 'field !': STATE_RESERVED }
     
     tr_start = False
@@ -99,12 +206,92 @@ class LibReservParser(HTMLParser):
         elif self.state == STATE_BARCODE:
             self.entry.append( ('barcode', data))
 
-class LibraryMashup:
-    API_ENTRY = "http://tremblefrog.org/libdb/query_isbn.php";  # we need our bridge for speed up again :(
-    TABLE_KEYWORDS = '<table width="100%" border="0" cellspacing="1" cellpadding="2" class="bibItems">'
-    TR_SPLIT = '<tr  class="bibItemsEntry">'
+class LibraryUserList:
+    def __init__(self, barcode, password, user_api='', query_api=''):
+        self.USER_API_ENTRY = user_api
+        self.QUERY_API_ENTRY = query_api
+        
+        self.barcode = barcode.strip()
+        self.password = password.strip()
+        self.authbody = None
+        self.user_parser = UserListParser()
+        self.isbn_parser = LibISBNParser()
+        
+        if self.barcode and self.password:
+            self.authbody = base64.b64encode("%s:%s" % (self.barcode, self.password))
     
-    parser = LibReservParser()
+    def get_user_list(self):
+        if not self.authbody:
+            return None
+            
+        response = self.post(self.USER_API_ENTRY, [('auth', self.authbody)])
+                    
+        self.user_parser.initialize()
+        self.user_parser.feed(response)
+        
+        return self.user_parser.user_list
+    
+    def retrieve_isbn(self, user_list):
+        """Retrieve corresponding ISBN number for books in the borrowing list.
+        Since ISBN is utilized as the unique 'KEY' in identifying and locating book on Douban,
+        we need to map the Call Numbers to ISBN serial number."""
+        
+        callno_list = [book['callno'] for book in user_list if 'callno' in book]
+        callno_concat = ",".join(callno_list)
+        response = self.post(self.QUERY_API_ENTRY, [('callno', callno_concat),
+            ('meta', 1)])
+        
+        isbn_dict = {}
+        response_group = response.split('<split />')
+        if response_group and len(response_group):
+            for idx in range(len(response_group)):
+                self.isbn_parser.initialize()
+                self.isbn_parser.feed(response_group[idx])
+                _isbn = self.isbn_parser.isbn
+                user_list[idx].setdefault('isbn', _isbn)
+                
+                isbn_dict.setdefault(_isbn, user_list[idx])
+                
+        return isbn_dict
+                
+    def check_updates(self, history_dict):
+        """Check for updates and differences of user's borrowing list
+        by comparing the historical set of books and newly synchronized one.
+        
+        Returns: One dict and two lists wrapped by a tuple:
+        ({SYNC_LIST}, [NEWLY_BORROWED], [RETURNED])"""
+        user_list = self.get_user_list()
+        if user_list:
+            isbn_indexed_dict = self.retrieve_isbn(user_list)
+            isbn_new_books = set(isbn_indexed_dict.keys())
+            isbn_history_books = set(history_dict.keys())
+            
+            set_intersection = isbn_new_books & isbn_history_books
+            return isbn_indexed_dict, list(isbn_new_books - isbn_history_books), list(isbn_history_books - set_intersection)
+            # return ({SYNC_LIST}, [NEWLY_BORROWED], [RETURNED])
+        else:
+            raise Exception, 'Failed to synchornize user list.'
+            
+    def post(self, uri, params):
+        param = urlencode(params)
+        try:
+            response = urllib2.urlopen(uri, param).read()
+        except:
+            return None
+        
+        return response
+    
+class LibraryMashup:
+    """This class inquiries metadata and reservation status through UESTC Library portal.
+    Given any valid and arbitrary ISBN serial, this class simulates an inquiry in library portal via HTTP.
+    Finally, after parsing raw HTML response, this class returns a list of books with required properties.
+    """
+    def __init__(self, api=''):
+        self.API_ENTRY = api
+        self.TABLE_KEYWORDS = '<table width="100%" border="0" cellspacing="1" cellpadding="2" class="bibItems">'
+        self.TR_SPLIT = '<tr  class="bibItemsEntry">'
+    
+        self.parser = LibReservParser()
     
     def query(self, isbn):
         if not isbn: return None
@@ -129,21 +316,3 @@ class LibraryMashup:
         self.parser.initialize()
         self.parser.feed(html)
         return (self.parser.count, self.parser.available, [dict(book) for book in self.parser.books])
-        
-    def library_inquiry(self, isbn):
-        params = ('isbn', )
-            
-        param = urlencode(params)
-        
-        
-        if '未找到' in response:   # not found
-            return None
-        else:
-            tbl_start = response.find(self.TABLE_KEYWORDS)
-            if tbl_start:
-                tbl_end = response.find('</table>', tbl_start) + len('</table>')
-                tbl_content = response[tbl_start:tbl_end]
-                
-                return tbl_content
-            else:
-                return None
